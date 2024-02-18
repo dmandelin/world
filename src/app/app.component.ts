e76a7498f20d2998029bbf24d3a708670abb34ff
Dave Mandelin <dmandelin@gmail.com>
2024-02-18 14:06:37 -0800

    Add blank map info panel.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { MapInfoPanelComponent } from './map-info-panel/map-info-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MapComponent],
+  imports: [RouterOutlet, MapComponent, MapInfoPanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/map-info-panel/map-info-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-info-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-info-panel.component.html',
+  styleUrl: './map-info-panel.component.scss'
+})
+export class MapInfoPanelComponent {
+
+}
