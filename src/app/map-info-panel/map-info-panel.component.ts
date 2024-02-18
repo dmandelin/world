27f2050a8f86defcd6c7af0f244ee986ca23c8f8
Dave Mandelin <dmandelin@gmail.com>
2024-02-18 15:30:13 -0800

    Populate info panel.

--- src/app/map-info-panel/map-info-panel.component.ts
+++ src/app/map-info-panel/map-info-panel.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { World } from '../world';
+import { NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-map-info-panel',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgStyle],
   templateUrl: './map-info-panel.component.html',
   styleUrl: './map-info-panel.component.scss'
 })
--- src/app/world.ts
+++ src/app/world.ts
@@ -3,50 +3,75 @@ import { Injectable } from "@angular/core";
 @Injectable({providedIn: 'root'})
 export class World {
     private year_ = 0;
-    private polities = [...DEFAULT_POLITIES];
-    readonly map = new WorldMap(5, 5);
+    private polities = DEFAULT_POLITIES.map(pd => new Polity(this, pd.name, pd.mapColor));
+    readonly map = new WorldMap(5, 5, this.polities);
+
+    constructor() {
+    }
 
     get year() { return this.year_; }
 
+    get population() {
+        return this.map.tiles.flat().map(t => t.population).reduce((a, b) => a + b);
+    }
+
     nextTurn() {
         this.year_ += 20;
-      }
+    }
     
-    //getPolitiesSortedByPopulation() {
-    //    return this.polities.sort((a, b) => b.population - a.population);
-    //}
+    getRankedPolities(): Polity[] {
+        return [...this.polities].sort((a, b) => {
+            const popDiff = a.population - b.population;
+            if (popDiff) return popDiff;
+            if (a.name < b.name) return -1;
+            if (b.name < a.name) return 1;
+            return 0;
+        })
+    }
 }
 
 class Polity {
+    constructor(private readonly world: World, readonly name: string, readonly mapColor: string) {}
+
+    get population() {
+        return this.world.map.tiles
+            .flat()
+            .filter(t => t.controller == this)
+            .map(t => t.population)
+            .reduce((a, b) => a + b);
+    }
+}
+
+class PolityDef {
     constructor(readonly name: string, readonly mapColor: string) {}
 }
 
 const DEFAULT_POLITIES = [
-    new Polity('A', 'red'),
-    new Polity('B', 'orange'),
-    new Polity('C', 'yellow'),
-    new Polity('D', 'green'),
-    new Polity('E', 'blue'),
-    new Polity('F', 'purple'),
-    new Polity('G', 'brown'),
-    new Polity('H', 'grey'),
-    new Polity('I', 'pink'),
-    new Polity('J', 'lightblue'),
-    new Polity('K', 'magenta'),
-    new Polity('L', 'cyan'),
-    new Polity('M', 'darkred'),
-    new Polity('N', 'lightgreen'),
-    new Polity('O', 'darkgreen'),
-    new Polity('P', 'darkblue'),
-    new Polity('Q', 'indigo'),
-    new Polity('R', 'darkorange'),
-    new Polity('S', 'tan'),
-    new Polity('T', 'slateblue'),
-    new Polity('U', 'violet'),
-    new Polity('V', 'mediumseagreen'),
-    new Polity('W', 'goldenrod'),
-    new Polity('X', 'darkgrey'),
-    new Polity('Y', 'lightgrey'),
+    new PolityDef('A', 'red'),
+    new PolityDef('B', 'orange'),
+    new PolityDef('C', 'yellow'),
+    new PolityDef('D', 'green'),
+    new PolityDef('E', 'blue'),
+    new PolityDef('F', 'purple'),
+    new PolityDef('G', 'brown'),
+    new PolityDef('H', 'grey'),
+    new PolityDef('I', 'pink'),
+    new PolityDef('J', 'lightblue'),
+    new PolityDef('K', 'magenta'),
+    new PolityDef('L', 'cyan'),
+    new PolityDef('M', 'darkred'),
+    new PolityDef('N', 'lightgreen'),
+    new PolityDef('O', 'darkgreen'),
+    new PolityDef('P', 'darkblue'),
+    new PolityDef('Q', 'indigo'),
+    new PolityDef('R', 'darkorange'),
+    new PolityDef('S', 'tan'),
+    new PolityDef('T', 'slateblue'),
+    new PolityDef('U', 'violet'),
+    new PolityDef('V', 'mediumseagreen'),
+    new PolityDef('W', 'goldenrod'),
+    new PolityDef('X', 'darkgrey'),
+    new PolityDef('Y', 'lightgrey'),
 ];
 
 class Tile {
@@ -62,12 +87,12 @@ class Tile {
 class WorldMap {
     tiles: Tile[][];
   
-    constructor(public width: number, public height: number) {
+    constructor(public width: number, public height: number, polities: ReadonlyArray<Polity>) {
         this.tiles = [];
         for (let i = 0; i < height; i++) {
             this.tiles[i] = [];
             for (let j = 0; j < width; j++) {
-                this.tiles[i][j] = new Tile(1000, DEFAULT_POLITIES[i * width + j])
+                this.tiles[i][j] = new Tile(1000, polities[i * width + j])
             }
         }
     }
