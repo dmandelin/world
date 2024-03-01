a3e946094487d77d71943aa446b7c6b0e7631105
Dave Mandelin <dmandelin@gmail.com>
2024-03-01 14:09:02 -0800

    Add linear borders.

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { World, Tile } from '../world';
-import { NgFor, NgStyle } from '@angular/common';
+import { Polity, World, Tile } from '../world';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [NgFor, NgStyle],
+  imports: [NgClass, NgFor, NgStyle],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss'
 })
@@ -15,5 +15,22 @@ export class MapComponent {
 
   popDisplay(tile: Tile): string {
     return `${Math.floor(tile.population/100)}/${Math.floor(tile.capacity/100)}`;
+  }
+
+  tileClasses(i: number, j: number): {[key: string]: boolean} {
+    const tt = this.world.map.tiles;
+    const ans = {
+      'tbt': i == 0 || this.group(tt[i-i][j]) != this.group(tt[i][j]),
+      'tbb': i == this.world.map.height - 1,
+      'tbl': j == 0 || this.group(tt[i][j-1]) != this.group(tt[i][j]),
+      'tbr': j == this.world.map.width - 1,
+    };
+    console.log(i, j, ans);
+    return ans;
+  }
+
+  group(tile: Tile): Polity {
+    const controller = tile.controller;
+    return controller.suzerain || controller;
   }
 }
