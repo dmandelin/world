c4f9202c96a6e56737ec3a0875a8c55374aa5a2f
Dave Mandelin <dmandelin@gmail.com>
2024-08-04 09:36:25 -0700

    Partial progress on outline for early political system.

---
+++ src/app/model/politics.ts
@@ -0,0 +1,57 @@
+// Outline
+//
+// Classically there are stages of political development like this:
+// - Egalitarian bands
+//   - typically immediate-return hunter-gatherers
+//   - status differences but not formalized
+//   - leadership by assent of each and all
+//   - traditional norms
+// - Achievement-based ("big man") societies
+//   - typically early hortculturalists
+//   - formal degrees of status can be achieved as an adult
+//   - leadership by assent of each and all, but emerging client relationships
+//   - traditional norms, but perhaps shaped more by big-man choices
+// - Ranked societies
+//   - typically agriculturalists
+//   - families are ranked and status depends heavily on birth
+//   - some degree of coercion
+//   - traditional norms, but some ability of chiefs and elites to alter them
+// - Stratified societies
+//   - typically agriculturalists with some degree of urbanization
+//   - formal classes and stations
+//   - coercion and exploitation generally available
+//   - traditional norms still apply but often overridden by authorities
+//
+// However, our goal is not to have a schematic progression, but rather to
+// model the transitions between these kinds of societies, and the causes
+// and effects in all power sources.
+//
+// Starting point: 5000 BC, ranked societies. Although it would be interesting
+// to model developments in simpler stages, and at some point we may, not that
+// much is happening just yet, so it will be more interesting to start with
+// somewhat more complex societies.
+//
+// Initial model for ranked societies:
+//
+// - Think about it in terms of 
+//   (a) political actions and decisions available
+//   (b) effects on other power sources and itself
+//   (c) causes of development of additional *single features* of stratified societies
+//       and the effects of those features
+//
+// - Political actions and decisions available
+// - Effects on other power sources and itself
+//   - Production: 
+//     - Coordination bonus to local tile production for all goods
+//       - Represents appropriate subsidy, trade regulation, dispute resolution
+//     - Ability to create larger irrigation works
+//   - Trade: 
+//     - Coordination bonus to local trade
+//     - Ability to coordinate long-distance trade, including with off-map trade points
+//   - Consumption: Utility bonus for redistribution
+//   - Technology: Bonuses due to scale, coordination, and subsidy/insurance
+//   - Religion: Bonus ability to claim labor
+//   - Military:
+//     - Coordination bonus to military actions, but relatively minor at this point
+//     - Ability to build fortifications
+// - Additional features that can develop and their causes and effects
